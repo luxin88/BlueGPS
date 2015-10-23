623198c406080095158fac998de7eacca3bbaa30
luxin <luxin@diaobaosq.com>
2015-10-23 09:11:28 +0800

    Initial Commit

---
+++ BlueGPS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BlueGPS
+//
+//  Created by luxin88 on 15/10/23.
+//  Copyright © 2015年 BG5TWC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BlueGPS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BlueGPS
+//
+//  Created by luxin88 on 15/10/23.
+//  Copyright © 2015年 BG5TWC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
